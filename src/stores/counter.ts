655fd5c345595409c5805591f8f743b64820f884
Eduardo San Martin Morote <posva13@gmail.com>
2021-10-28 16:45:40 +0200

    add basic project

---
+++ src/stores/counter.ts
@@ -0,0 +1,9 @@
+import { defineStore, acceptHMRUpdate } from 'pinia'
+
+export const useCounter = defineStore('counter', {
+  state: () => ({ n: 0 }),
+})
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useCounter, import.meta))
+}
